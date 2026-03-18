b293b416fe1abee3924394bc4e93f8b53a644765
Laboratorio Cisco <labcisco@uam.net.ni>
2026-03-18 00:14:31 -0600

    Ejercicio 5

---
+++ src/run/clasificadorcontrasena.java
@@ -0,0 +1,46 @@
+package run;
+
+import java.util.Scanner;
+
+public class clasificadorcontrasena {
+
+    public static String clasificar(String password) {
+
+        if (password.length() < 8) {
+            return "Insegura";
+        }
+
+        boolean tieneNumero = false;
+        boolean tieneMayuscula = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                tieneNumero = true;
+            }
+            if (Character.isUpperCase(c)) {
+                tieneMayuscula = true;
+            }
+        }
+
+        if (tieneNumero && tieneMayuscula) {
+            return "Fuerte";
+        } else if (tieneNumero) {
+            return "Moderada";
+        } else {
+            return "Insegura";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa una contraseña: ");
+        String password = scanner.nextLine();
+
+        String resultado = clasificar(password);
+        System.out.println("Clasificación: " + resultado);
+
+        scanner.close();
+    }
+}
+
